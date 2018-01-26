dbb2db80b9d93bea13cca63daa0428d5e61200f1
Najah Izquierdo <najah.izquierdo@yahoo.com>
2018-01-26 13:56:33 -0800

    Add styling

--- src/SidebarComponent.js
+++ src/SidebarComponent.js
@@ -7,24 +7,25 @@ class Sidebar extends Component {
   render() {
     return(
 <div className="col s10 m4 l3">
-       <div className="card-panel grey">
+       <div className="card-panel grey lighten-1">
          <div className="pictures">
            <center>
             <img src={logo} className="icons"/><p>
-            <img src={socialmedia} className="icons" usemap="#image-map" />
-
-          <map name="image-map">
-              <area href="https://www.youtube.com/user/hugoizquierdo" coords="12,13,85,66" shape="rect" />
-              <area href="https://www.facebook.com/blazingstonepizza/" coords="110,8,159,71" shape="rect" />
-              <area href="https://www.instagram.com/blazingstonepizza/" coords="175,7,244,70" shape="rect" />
-          </map>
-          </p>
 
 
           <p> 3501 Monterey Rd </p>
           <p> Los Angeles, CA 90042 </p>
           (323)309-6527
-            </center>
+          <p>
+            <img src={socialmedia} className="icons" usemap="#image-map" />
+            <map name="image-map" >
+              <area href="https://www.youtube.com/user/hugoizquierdo/videos?sort=dd&shelf_id=0&view=0" coords="12,13,85,66" shape="rect" />
+              <area href="https://www.facebook.com/blazingstonepizza/" coords="110,8,159,71" shape="rect" />
+              <area href="https://www.instagram.com/blazingstonepizza/" coords="175,7,244,70" shape="rect" />
+            </map>
+  </p>
+</p>
+</center>
 
             <p>
             <YelpReviews />
