99c5a78de7017b03ab7aebe193d7e78aca00ee71
Najah Izquierdo <najah.izquierdo@yahoo.com>
2018-02-01 20:25:03 -0800

    Add scalability

--- src/YelpReviews.js
+++ src/YelpReviews.js
@@ -5,19 +5,8 @@ import Yelp from 'yelp';
 class YelpReviews extends Component {
   render() {
     return(
-      <div>
-        <div className="yelp-design">
-        </div>
-          <iframe data-review-id="e3D2Pskn5IaO0juEK88sSQ" className="yelp-review yelp"
-          src="https://www.yelp.com/embed/review/e3D2Pskn5IaO0juEK88sSQ"
-          scrolling="yes" ></iframe> _______________________________
-          <iframe data-review-id="q_mKIvZyKs8XdmqMtMeiwA" className="yelp-review yelp"
-          src="https://www.yelp.com/embed/review/q_mKIvZyKs8XdmqMtMeiwA"
-          scrolling="yes" ></iframe> _______________________________
-          <iframe data-review-id="bbuK0BCC7J3qI1ZqPrfnSQ" className="yelp-review yelp"
-          src="https://www.yelp.com/embed/review/bbuK0BCC7J3qI1ZqPrfnSQ"
-          scrolling="yes" ></iframe>
 
+        <div className="yelp-design">
 
       </div>
 
