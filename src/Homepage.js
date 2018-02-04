d971bcd4660f3a7791987fd42013f73e1d568b37
Najah Izquierdo <najah.izquierdo@yahoo.com>
2018-02-03 20:01:43 -0800

    add carousel and fix picture sizes

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,6 +5,7 @@ import porkys from './porkys.png'
 import dessert from './dessert.png'
 import car from './car.png'
 import lasagna from './lasagna.png'
+import Slider from 'react-slick';
 
 class Homepage extends Component {
 
@@ -14,23 +15,29 @@ class Homepage extends Component {
   arrows: true,
   draggable: true,
   swipeToSlide: true,
-  speed: 500,
+  speed: 1000,
   adaptiveHeight: true,
-  slidesToShow: 1,
-  style: {
-    height: "40%"
-
-  }
+  slidesToShow: 1
 };
     return(
       <div className="page jumbotron">
-          <div><img src={porkys} className="main-announcement"/></div>
+        <div className="desktop-homepage hidden-xs">
+        <Slider {...settings}>
+        <div><img src={porkys} className="pictures"/></div>
+        <div><img src={footballfield} className="pictures" /></div>
+        <div><img src={lasagna} className="pictures"/></div>
+        <div><img src={car} className="pictures"/></div>
+        <div><img src={dessert} className="pictures"/></div>
+      </Slider>
+    </div>
 
+          <div className="mobile-homepage visible-xs">
+            <img src={porkys} className="main-announcement"/>
             <img src={footballfield} className="bottomthree" />
+            <img src={lasagna} className="bottomthree"/>
             <img src={car} className="bottomthree"/>
             <img src={dessert} className="bottomthree"/>
-            <img src={lasagna} className="bottomthree"/>
-
+            </div>
 
       </div>
     );
