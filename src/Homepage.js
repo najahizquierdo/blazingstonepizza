25d601f7e5b6b2199d3d0a6c977faa25199052bf
Najah <najahizquierdo@gmail.com>
2018-11-18 14:01:18 -0800

    New announcements

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Button, Card, Row, Col } from 'react-materialize';
-import porkys from './porkys.png'
-import car from './car.png'
+import carrotcake from './carrotcake.png'
+import stuffed from './stuffed.png'
+import baked from './baked.png'
+import chilibowl from './chilibowl.png'
 import lasagna from './lasagna.png'
-import deepdish from './deepdish.png'
 import Slider from 'react-slick';
 
 class Homepage extends Component {
@@ -23,8 +24,10 @@ class Homepage extends Component {
       <div className="page jumbotron">
         <div className="desktop-homepage hidden-xs">
         <Slider {...settings}>
-        <div><img src={deepdish} className="pictures"/></div>
-        <div><img src={porkys} className="pictures"/></div>
+        <div><img src={carrotcake} className="pictures"/></div>
+        <div><img src={stuffed} className="pictures"/></div>
+        <div><img src={chilibowl} className="pictures"/></div>
+        <div><img src={baked} className="pictures"/></div>
         <div>
         <iframe className="video"
         src="https://www.youtube.com/embed/YSKflgXXhu0"
@@ -33,17 +36,18 @@ class Homepage extends Component {
          allowfullscreen>
          </iframe>
         </div>
-        <div><img src={car} className="pictures"/></div>
+        <div><img src={lasagna} className="pictures"/></div>
       </Slider>
     </div>
 
           <div className="mobile-homepage visible-xs">
-          <img src={deepdish} className="main-announcement"/>
-            <img src={porkys} className="main-announcement"/>
+          <img src={carrotcake} className="main-announcement"/>
+            <img src={stuffed} className="main-announcement"/>
             <iframe className="mobile-video" src="https://www.youtube.com/embed/YSKflgXXhu0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
           </iframe>
+            <img src={chilibowl} className="bottomthree"/>
+            <img src={baked} className="bottomthree"/>
             <img src={lasagna} className="bottomthree"/>
-            <img src={car} className="bottomthree"/>
             </div>
 
       </div>
